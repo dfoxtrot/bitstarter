1a83aca88c4298fb6a274fc10225711427d28544
dfoxtrot <david.frezzato@gmail.com>
2013-09-05 12:05:29 +0000

    require fs

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 var buffer = new Buffer(fs.readFile(index.html),"utf-8");
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(buffer));
+  response.send(buffer.toString("utf-8", 0, 12));
 });
 
 var port = process.env.PORT || 5000;
