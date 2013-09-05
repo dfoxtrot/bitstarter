033d0c2f5d93780f4d38eb8d18e482ef73339782
dfoxtrot <david.frezzato@gmail.com>
2013-09-05 12:31:44 +0000

    test

--- web.js
+++ web.js
@@ -3,10 +3,11 @@ var app = express();
 var fs = require('fs');
 var buffer = new Buffer(16);
 var buffer2 = fs.readFileSync('index.html');
+var texte = buffer2.toString('utf-8');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('toto');
+  response.send(texte);
 });
 
 var port = process.env.PORT || 5000;
